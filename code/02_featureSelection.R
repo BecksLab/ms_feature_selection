2394efc6a904c6d69df4db1b0460acff6d54ebbc
Tanya Strydom <tanya.strydom@icloud.com>
2025-05-22 12:04:16 +0100

    🐣 get vermaat results

--- code/02_featureSelection.R
+++ code/02_featureSelection.R
@@ -4,6 +4,7 @@ library(corrplot)
 library(FactoMineR)
 library(here)
 library(psych)
+library(ggrepel)
 library(tidyverse)
 
 setwd(here::here())
@@ -85,39 +86,71 @@ ggplot() +
 
 # 2: look at correlation first
 
-pca <- PCA(topology_vermaat_subset)
+pca <- PCA(topology_vermaat, scale.unit = TRUE)
 
-# find number of dims describing 90 % of variance
-dim_num <- pca$eig %>%
-  as.data.frame() %>%
-  filter(`cumulative percentage of variance` < 90) %>%
-  nrow() %>% as.numeric()
-
-dim_descrip <- dimdesc(pca, axes = 1:5)
-
-# assign broader categories
-
-grps <- 
-  tibble(var = row.names(pca[["var"]][["coord"]])) %>%
-  reframe(cat = case_when(var %in% c("S1", "S2", "S5", "S4") ~ "Motif",
-                          var %in% c("richness", "links", "connectance", "complexity", "l_S", "ρ") ~ "Structure",
-                          var %in% c("diameter", "TL", "path", "ChLen", "ChNum", "top", "basal", "distance") ~ "Energy",
-                          var %in% c("GenSD", "VulSD", "omnivory", "herbivory", "intermediate", "centrality") ~ "Node"))
-
+# get variance explained for labels
 variance = pca$eig %>%
   as.data.frame() %>%
   pull(`percentage of variance`)
 
-# try plot as points
 
+# pull all vars signif correlated with first three dims
+dim_descrip <- dimdesc(pca, axes = 1:3)
+
+signif_corrs <- 
+  dim_descrip[["Dim.1"]] %>%
+  as.data.frame() %>%
+  rownames_to_column(., var = "var") %>%
+  mutate(dimension = "Dim.1") %>%
+  rbind(dim_descrip[["Dim.2"]] %>%
+          as.data.frame() %>%
+          rownames_to_column(., var = "var") %>%
+          mutate(dimension = "Dim.2")) %>%
+  rbind(dim_descrip[["Dim.3"]] %>%
+          as.data.frame() %>%
+          rownames_to_column(., var = "var") %>%
+          mutate(dimension = "Dim.3")) %>%
+  mutate(quanti.correlation = round(quanti.correlation, digits = 2))
+
+pca$var$cor %>%
+  as.data.frame() %>%
+  rownames_to_column(., var = "var") %>%
+  select(var, Dim.1, Dim.2, Dim.3) %>%
+  pivot_longer(!var,
+               names_to = "dimension",
+               values_to = "quanti.correlation") %>%
+  mutate(quanti.correlation = round(quanti.correlation, digits = 2)) %>%
+  full_join(signif_corrs) %>%
+  mutate(dimension = case_when(dimension == "Dim.1" ~ paste("PCA 1 (", round(variance[1]), "%)", sep = ""),
+                               dimension == "Dim.2" ~ paste("PCA 2 (", round(variance[2]), "%)", sep = ""),
+                               dimension == "Dim.3" ~ paste("PCA 3 (", round(variance[3]), "%)", sep = "")),
+         quanti.correlation = case_when(quanti.correlation > 0.53 & quanti.correlation <= 0.53 & quanti.p.value <= 0.05 ~ paste("**", quanti.correlation, "**", sep = ""),
+                                        quanti.correlation > 0.66 & quanti.p.value <= 0.01 ~ paste("**", quanti.correlation, "**", sep = ""),
+                                        .default = as.character(quanti.correlation))) %>%
+  select(!quanti.p.value) %>%
+  pivot_wider(names_from = dimension,
+              values_from = quanti.correlation) %>%
+  write.csv(.,
+            "../tables/vermaat_corr.csv",
+            row.names = FALSE)
+
+# plot pca
 ggplot(data = pca[["var"]][["coord"]]) +
+  geom_hline(yintercept = 0,
+             colour = "grey60",
+             linetype = 2) +
+  geom_vline(xintercept = 0,
+             colour = "grey60",
+             linetype = 2) +
   geom_point(aes(x = Dim.1,
-                 y = Dim.2,
-                 colour = grps$cat)) +
+                 y = Dim.2)) +
+  geom_text_repel(aes(x = Dim.1,
+                      y = Dim.2,
+                      label = row.names(pca[["var"]][["coord"]]))) +
   theme_classic() +
-  geom_hline(yintercept = 0) +
-  geom_vline(xintercept = 0) +
   lims(x= c(-1, 1), y = c(-1, 1)) +
-  labs(x = paste("PCA 1 ", round(variance[1]), "%",sep = ""),
-       y = paste("PCA 2 ", round(variance[2]), "%",sep = ""))
-  
+  labs(x = paste("PCA 1 (", round(variance[1]), "%)",sep = ""),
+       y = paste("PCA 2 (", round(variance[2]), "%)",sep = ""))
+
+ggsave("../figures/pca_vermaat.png")
+
