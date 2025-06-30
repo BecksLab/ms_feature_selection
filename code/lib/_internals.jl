86422ffa717c711d52e93c5de66aa360bad806b7
Tanya Strydom <tanya.strydom@icloud.com>
2025-06-30 10:16:58 +0100

    🐛 fix motif counts

--- code/lib/_internals.jl
+++ code/lib/_internals.jl
@@ -1,5 +1,6 @@
 # General sundry internal functions
 
+using LinearAlgebra
 using SpeciesInteractionNetworks
 using Statistics
 
@@ -50,10 +51,22 @@ function _network_summary(N::SpeciesInteractionNetwork{<:Partiteness,<:Binary})
         :ChNum => log(length(cl)),
         :path => mean(pathlengths(N)),
         :LinkSD => std(values(degree(N))) / l_s,
-        :S1 => length(findmotif(motifs(Unipartite, 3)[1], N)),
-        :S2 => length(findmotif(motifs(Unipartite, 3)[2], N)),
-        :S4 => length(findmotif(motifs(Unipartite, 3)[4], N)),
-        :S5 => length(findmotif(motifs(Unipartite, 3)[5], N)),
+        :S1 =>
+            length(
+                findmotif(motifs(Unipartite, 3)[1], remove_cannibals(N)),
+            )/(richness(N)^2),
+        :S2 =>
+            length(
+                findmotif(motifs(Unipartite, 3)[2], remove_cannibals(N)),
+            )/(richness(N)^2),
+        :S4 =>
+            length(
+                findmotif(motifs(Unipartite, 3)[4], remove_cannibals(N)),
+            )/(richness(N)^2),
+        :S5 =>
+            length(
+                findmotif(motifs(Unipartite, 3)[5], remove_cannibals(N)),
+            )/(richness(N)^2),
         :ρ => spectralradius(N),
         :centrality => mean(collect(values(centrality(N)))),
         :loops => loops(N),
@@ -70,14 +83,14 @@ _get_matrix(N::SpeciesInteractionNetwork)
     Internal function to return a matrix of interactions from a
     SpeciesInteractionNetwork
 """
-function _get_matrix(N::SpeciesInteractionNetwork)
+function _get_matrix(N::SpeciesInteractionNetwork{<:Partiteness,<:Binary})
 
     species = richness(N)
-    n = zeros(Int64, (species, species))
+    n = zeros(Bool, (species, species))
     for i in axes(n, 1)
         for j in axes(n, 2)
             if N.edges[i, j] == true
-                n[i, j] = 1
+                n[i, j] = true
             end
         end
     end
@@ -279,4 +292,22 @@ function loops(N::SpeciesInteractionNetwork)
     else
         return 0.0
     end
+end
+
+"""
+remove_cannibals(N::SpeciesInteractionNetwork{<:Partiteness,<:Binary})
+
+    Identifies and sets cannibalistic link to zero
+"""
+function remove_cannibals(N::SpeciesInteractionNetwork{<:Partiteness,<:Binary})
+
+    A = _get_matrix(N)
+
+    A[diagind(A)] .= false
+
+    nodes = Unipartite(species(N))
+    edges = Binary(A)
+    network = SpeciesInteractionNetwork(nodes, edges)
+
+    return network
 end
